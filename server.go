92ad6cae68f376d57d8d1da8e750877173609c26
Max Roy <max+unset@github.com>
2023-08-14 23:34:17 +0200

    JSON response

--- server.go
+++ server.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-    "net/http"
-    
-    "github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-    e.Logger.Fatal(e.Start(":8080"))
+	ip := os.Getenv("IP")
+	pod := os.Getenv("POD")
+	node := os.Getenv("NODE")
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.GET("/", func(c echo.Context) error {
+		res := map[string]interface{}{
+			"message":   "Hello from backend version v1.0.0",
+			"timestamp": time.Now().UTC().Format(time.RFC3339),
+			"ip":        ip,
+			"pod":       pod,
+			"node":      node,
+		}
+
+		// Return mock data as JSON response
+		return c.JSON(http.StatusOK, res)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
